da9362d8de84d3dad87f87149eff311ec4cb2ed6
Chris <Chris@Chris-PC.fios-router.home>
2018-10-08 14:04:08 -0400

    test

---
+++ src/paystation/PaystationMain.java
@@ -0,0 +1,26 @@
+package paystation;
+
+import java.util.HashMap.*;
+import java.util.Map.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class PaystationMain{
+    public static void main(String args[]){
+        int input = 0;
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please enter one of the following options:" +
+        "\n1)Deposit coins\n2)Display\n3)Buy ticket\n4)cancel\n5)Change rate Strategy \n\n");
+        input = reader.nextInt();
+//        while(){
+//            Scanner reader = new Scanner(System.in);
+//            System.out.println("Please enter one of the following options:" +
+//                    "\n1)Deposit coins\n2)Display\n3)Buy ticket\n4)cancel\n5)Change rate Strategy \n\n");
+//            input = reader.nextInt();
+//
+//        }
+//test
+    }
+}
+
